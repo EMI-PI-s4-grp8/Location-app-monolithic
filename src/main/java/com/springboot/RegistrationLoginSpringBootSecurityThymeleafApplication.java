b1ee916071385218badc59ed0cab0eeafcc62d38
reda-coder <redaelouahabi98@gmail.com>
2021-05-03 10:27:02 +0000

    fixing bugs

--- src/main/java/com/springboot/RegistrationLoginSpringBootSecurityThymeleafApplication.java
+++ src/main/java/com/springboot/RegistrationLoginSpringBootSecurityThymeleafApplication.java
@@ -2,11 +2,12 @@ package com.springboot;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-
+ 
 import com.springboot.model.Mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
