91b16ebd841f8d56904694027d6bdebc66783163
zakaria-bouaouda <zakariaouaouda1@gmail.com>
2021-05-18 01:02:44 +0100

    OK

--- src/main/java/com/springboot/model/Logement.java
+++ src/main/java/com/springboot/model/Logement.java
@@ -24,7 +24,7 @@ public class Logement {
 	private String intitule;
 	@NotEmpty
 	private String description;
-	@Size(min=20,max=10000)
+	//@Size(min=20,max=10000)
 	private double prix;
 	@Size(min=5,max=100)
 	private String adresse;
